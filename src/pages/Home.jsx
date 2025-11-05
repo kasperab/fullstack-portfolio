5429e88c41d666981d2d82bd46dd7cb4b94b8ad8
kasperab <kasperab@yahoo.se>
2025-11-05 14:55:01 +0100

    Changed links on Home page into images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import cakery from "./img/cakery.png";
+import todolist from "./img/todolist.png";
 
 export default function Home() {
     return (
         <div>
-            <Link to="/cakery">Cakery</Link>
-            <br/>
-            <Link to="/todolist">ToDoList</Link>
+            <Link to="/cakery">
+                <img src={cakery} alt="Cakery" className="w-100" />
+            </Link>
+            <br /><br />
+            <Link to="/todolist">
+                <img src={todolist} alt="ToDoList" className="w-100" />
+            </Link>
         </div>
-
     );
 }
