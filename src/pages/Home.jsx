1b83845eecebaa0f7fdf0fd106c4601f7429f661
kasperab <kasperab@yahoo.se>
2025-11-05 15:17:43 +0100

    Added about text to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,16 @@ export default function Home() {
             <Link to="/todolist">
                 <img src={todolist} alt="ToDoList" className="w-100" />
             </Link>
+            <br /><br />
+            <h3>About me</h3>
+            <p>
+                I've been programming for over 10 years and have in that time used several different languages.
+                Programming and creative problem solving is something I enjoy a lot.
+                Most recently, I have been starting to learn fullstack programming.
+                On this website you will find some of my recent projects, using C#, HTML, CSS, JavaScript, and React.
+                The website itself is also made using React and Bootstrap.
+                You can find more of my projects on <a href="https://github.com/kasperab" target="_blank">GitHub</a> and connect with me on <a href="https://linkedin.com/in/kasperab" target="_blank">LinkedIn</a>.
+            </p>
         </div>
     );
 }
